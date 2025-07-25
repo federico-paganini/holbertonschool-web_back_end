c3eeeaa6ebdf7c8f7e7fdc9376925021e6784fcb
federico-paganini <perfedefer@gmail.com>
2025-07-25 19:36:23 -0300

    fix: Line length

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -5,7 +5,8 @@ This module provides a simple addition function.
 It exports a single function:
     - add(a, b): Returns the sum of two floating-point numbers.
 
-Intended for basic arithmetic operations or as a utility function in larger programs.
+Intended for basic arithmetic operations or as
+a utility function in larger programs.
 """
 
 
