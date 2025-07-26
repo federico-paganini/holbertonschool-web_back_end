581b6c83c364e025684b94b264ba16a97a558f49
federico-paganini <perfedefer@gmail.com>
2025-07-26 17:40:59 -0300

    feat: 5-sum_list.py

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env Python3
+"""A module to sum a list of numbers using type annotations."""
+
+
+def sum_list(nums: list[float]) -> float:
+    """
+    Sums a list of numbers.
+
+    :param numbers: A list of float numbers to sum.
+    :return: The sum of the numbers in the list.
+    """
+    return sum(nums)
