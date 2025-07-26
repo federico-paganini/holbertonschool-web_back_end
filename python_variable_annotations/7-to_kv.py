777454d0f328a2af624263522a6a01963a191cdd
federico-paganini <perfedefer@gmail.com>
2025-07-26 17:59:28 -0300

    feat: 7-to_kv.py

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This module defines a function to_kv that takes a key and a value,
+squares the value, and returns a tuple containing the key and the squared value.
+"""
+from typing import Tuple, Union
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Convert a key and value into a tuple."""
+    return (k, float(v**2))
