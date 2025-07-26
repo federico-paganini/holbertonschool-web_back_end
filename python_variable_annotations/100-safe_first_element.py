86c5ced7e929784a5b4d288876b811933ceb3d37
federico-paganini <perfedefer@gmail.com>
2025-07-26 19:55:18 -0300

    feat: 100-safe_first_element.py

---
+++ python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Safe first element of a list with type annotations.
+This module provides a function to safely retrieve
+the first element of a list,
+returning None if the list is empty.
+"""
+from typing import List, Any, Optional
+
+
+def safe_first_element(lst: List[Any]) -> Optional[Any]:
+    """
+    Returns the first element of a list if it exists,
+    otherwise returns None.
+    :param lst: List from which to retrieve the first element
+    :return: The first element of the list or None if the list is empty
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
