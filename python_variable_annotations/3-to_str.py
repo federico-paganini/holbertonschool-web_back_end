32ee2b7dbd128aa26a379bad406bee1fe13faded
federico-paganini <perfedefer@gmail.com>
2025-07-25 22:52:08 -0300

    feat: 3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""This module provides a simple to_str function."""
+
+
+def to_str(n: float) -> str:
+    """Convert a floating-point number to a string.
+
+    Args:
+        n (float): The number to convert.
+
+    Returns:
+        str: The string representation of the number.
+    """
+    return str(n)
