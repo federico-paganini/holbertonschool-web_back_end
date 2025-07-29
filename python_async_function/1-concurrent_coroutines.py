f492f89c4d1cbce9df50585e96bd157bbf8c722e
federico-paganini <perfedefer@gmail.com>
2025-07-29 18:49:12 -0300

    fix: Import, old type hinting

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,10 +6,11 @@ It uses the `wait_random` coroutine from another module to simulate asynchronous
 with random delays. The main function, `wait_n`, launches multiple such tasks concurrently.
 """
 import asyncio
-wait_random = __import__("0-basic_async_syntax.py").wait_random
+from typing import List
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawn `n` coroutines that each wait for a random delay between 0 and `max_delay` seconds.
 
