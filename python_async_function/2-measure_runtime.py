0e7b054d4ecad849eba634049b05f5da1fa6c293
federico-paganini <perfedefer@gmail.com>
2025-07-29 19:21:23 -0300

    fix: Not an async function

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -7,7 +7,7 @@ import asyncio
 import time
 wait_n = __import__("1-concurrent_coroutines").wait_n
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
     Measure the average time per coroutine execution for wait_n.
 
