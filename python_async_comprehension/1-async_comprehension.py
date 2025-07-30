17748ec075976c5224e4d0618a790fff7b6fe30e
federico-paganini <perfedefer@gmail.com>
2025-07-29 23:05:25 -0300

    feat: 1-async_comprehension.py

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from typing import List
+import asyncio
+"""
+Module: 1-async_comprehension
+Consumes values from an asynchronous generator and returns them as a list.
+"""
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Collects 10 random floats from the async_generator using async comprehension.
+
+    Returns:
+        List[float]: A list of 10 floats yielded by the asynchronous generator.
+    """
+    results = []
+    async for values in async_generator():
+        results.append(values)
+
+    return results
