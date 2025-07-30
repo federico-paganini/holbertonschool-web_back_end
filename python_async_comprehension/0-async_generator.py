33867de18f90e2377dd002ba450f701b12a65c43
federico-paganini <perfedefer@gmail.com>
2025-07-29 22:40:10 -0300

    fix: Tye hint (?

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from typing import AsyncGenerator
+from typing import Generator
 import random
 import asyncio
 """
@@ -10,7 +10,7 @@ The `async_generator` coroutine loops 10 times, each time awaiting an
 asynchronous 1-second delay, then yielding a random float between 0 and 10.
 """
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Asynchronous generator that yields random floats between 0 and 10.
 
