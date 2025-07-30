edfd2ef87b48e49843c2c2949f9159a04d912993
federico-paganini <perfedefer@gmail.com>
2025-07-29 23:30:02 -0300

    fix: gather import (Fucking checker)

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -3,7 +3,7 @@
 This module provides a coroutine to measure the runtime
 of executing multiple asynchronous comprehensions concurrently.
 """
-import asyncio
+from asyncio import gather
 import time
 async_comprehension = __import__("1-async_comprehension").async_comprehension
 
@@ -16,12 +16,12 @@ async def measure_runtime() -> float:
         float: The total elapsed time in seconds.
     """
     start_time = time.perf_counter()
-    await asyncio.gather(
+    await gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
     )
     end_time = time.perf_counter()
-    
+
     return end_time - start_time
