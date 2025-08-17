c3f6d2c754b3cdcf007b5d1fa715c72f996a17b5
federico-paganini <perfedefer@gmail.com>
2025-08-17 20:16:07 -0300

    feat: task 09

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Module: insert_school_module
+----------------------------
+This module provides a function to insert a document into a MongoDB collection
+using PyMongo and return the ObjectId of the inserted document.
+
+Function:
+    insert_school(mongo_collection, **kwargs) -> str
+"""
+
+
+def insert_school(mongo_collection, **kwargs) -> str:
+    """
+    Insert a document into the given MongoDB collection.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection): The PyMongo
+        Collection object where the document will be inserted.
+        **kwargs: Arbitrary key-value pairs representing
+        the fields of the document.
+
+    Returns:
+        str: The ObjectId of the inserted document as a string.
+    """
+    data = dict(**kwargs)
+    result = mongo_collection.insert_one(data)
+    return str(result.inserted_id)
