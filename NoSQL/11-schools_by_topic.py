7ecea7191fec5ae36b9fcb12d79d6f9ffc888245
federico-paganini <perfedefer@gmail.com>
2025-08-17 21:16:16 -0300

    feat: task 11

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
+"""
+Module: schools_by_topic
+------------------------
+Function to return all schools that have a given topic in their 'topics' list.
+"""
 
-def schools_by_topic(mongo_collection, topic) -> list[dict]:
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Find all documents in the collection where 'topics' contains the given topic.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection): The MongoDB collection.
+        topic (str): The topic to search for.
+    """
     result = mongo_collection.find(
         { "topics": topic }
     )
