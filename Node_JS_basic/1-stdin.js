fa91d4bec33804d721ffb21c56d4263a84634aa6
federico-paganini <perfedefer@gmail.com>
2025-09-05 20:24:34 -0300

    fix: Output

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 console.log("Welcome to Holberton School, what is your name?");
 
 process.stdin.on("data", (data) => {
-  const input = data.toString().trim();
+  const input = data.toString();
   console.log(`Your name is: ${input}`);
   process.exit();
 });
