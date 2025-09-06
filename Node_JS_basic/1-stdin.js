cd0503e527792fe5c21f8a622e21402947b4c273
federico-paganini <perfedefer@gmail.com>
2025-09-05 21:24:35 -0300

    fix: ????

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,10 @@
-console.log("Welcome to Holberton School, what is your name?");
+rocess.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-process.stdin.on("data", (data) => {
-  const input = data.toString().trim();
-  console.log(`Your name is: ${input}`);
-  process.exit();
+process.stdin.on("readable", () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
 process.on("exit", () => {
