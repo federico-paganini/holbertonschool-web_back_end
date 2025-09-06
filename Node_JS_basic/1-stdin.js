256a5da60e47ef6b01fc66ce3fb2bf17a277eb0a
federico-paganini <perfedefer@gmail.com>
2025-09-05 21:57:57 -0300

    fix: Lint

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', data => {
+process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
 });
 
