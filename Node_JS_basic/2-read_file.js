0e2a7f868de6950167e21c8234ae92166ddf03be
federico-paganini <perfedefer@gmail.com>
2025-09-07 11:42:19 -0300

    fix: Lint

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -34,8 +34,8 @@ function countStudents(path) {
   for (const [field, names] of Object.entries(fields)) {
     console.log(
       `Number of students in ${field}: ${names.length}. List: ${names.join(
-        ', '
-      )}`
+        ', ',
+      )}`,
     );
   }
 }
