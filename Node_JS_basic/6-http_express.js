b59c52a1a1edf877e3de573e3975e1d22104cae6
federico-paganini <perfedefer@gmail.com>
2025-09-07 12:41:51 -0300

    feat: task_06

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,11 @@
+const express = require(`express`);
+const app = express();
+const port = 1245;
+
+app.get('/', (_, res) => {
+  res.type('text/plain').send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running in http://localhost:${port}`);
+});
