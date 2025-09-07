02c7e69469f804cb89292f39783065ba479d3ed3
federico-paganini <perfedefer@gmail.com>
2025-09-07 12:21:38 -0300

    feat: 5-http.js

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const database = process.argv[2];
+
+const app = http.createServer((_, res) => {
+  const sendResponse = (data) => {
+    res.write(data);
+  };
+
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  if (_.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (_.url === '/students') {
+    res.write('This is the list of our students\n');
+
+    const originalLog = console.log;
+    console.log = (...args) => {
+      res.write(args.join(' ') + '\n');
+    };
+
+    countStudents(database)
+      .then(() => {
+        console.log = originalLog;
+        res.end();
+      })
+      .catch(() => {
+        console.log = originalLog;
+        res.end('Cannot load the database');
+      });
+  } else {
+    res.end('Hello Holberton School!');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
